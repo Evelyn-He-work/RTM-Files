69af1d84a5d1b2488f798025655573cbc63ebc51
Evelyn He <Evelyn.He@rideautransitgroup.ca>
2022-06-16 15:59:44 -0400

    minor changes - variable name changes

--- fileAlstom.ps1
+++ fileAlstom.ps1
@@ -1,36 +1,42 @@
+#setting up + variable declaration
 $excel = New-Object -ComObject Excel.Application
 $excel.visible = $true
 $path = 'C:\Evelyn Documents\RTM Missing Letters\2019.xlsx'
 $excel.Workbooks.Open($path)
 $file = $excel.Workbooks.Item("2019")
 $all = $file.Worksheets.Item("All")
-$alstom = $file.Worksheets.Item("Alstom")
-$alstomDesc = $file.Worksheets.Item("AlstomDesc")
+$og = $file.Worksheets.Item("Contracts & Suppliers")
+$new = $file.Worksheets.Item("Contracts & Suppliers Desc")
 $nameRange = $all.Range("A2").EntireColumn
-#$verRange=$all.Range("C2").EntireColumn
-for ($i = 2; $i -le 1494; $i++) {
-    $currName = $alstom.Cells.Item($i, 1).Value()
-    $currVersion = $alstom.Cells.Item($i, 2).Value()
+
+#Loop through required documents rows
+for ($i = 745; $i -le 15625; $i++) {
+    $currName = $og.Cells.Item($i, 1).Value()
+    $currVersion = $og.Cells.Item($i, 2).Value()
+
+    #Finds name in document with all letter rows
     $searchName = $nameRange.find($currName)
     if ($null -ne $searchName) {
         $row = $searchName.Row
         Write-Host $row
+
+        #make sure versions match & write information on new sheet
         if ($currVersion -eq $all.Cells.Item($row,3).Value()) {
-            $alstomDesc.Cells.Item($i, 1) = $currName
-            $alstomDesc.Cells.Item($i, 3) = $currVersion
-            $alstomDesc.Cells.Item($i, 2) = $all.Cells.Item($row, 2).Value()
+            $new.Cells.Item($i, 1) = $currName
+            $new.Cells.Item($i, 3) = $currVersion
+            $new.Cells.Item($i, 2) = $all.Cells.Item($row, 2).Value()
         }
         else {
-            $alstomDesc.Cells.Item($i, 1) = $currName
-            $alstomDesc.Cells.Item($i, 3) = $currVersion
-            $alstomDesc.Cells.Item($i, 2) = "No Description"
-            $alstomDesc.Cells.Item($i,2).Font.ColorIndex="38"
+            $new.Cells.Item($i, 1) = $currName
+            $new.Cells.Item($i, 3) = $currVersion
+            $new.Cells.Item($i, 2) = "No Description"
+            $new.Cells.Item($i,2).Font.ColorIndex="38"
         }
     } else{
-        $alstomDesc.Cells.Item($i,1)=$currName
-        $alstomDesc.Cells.Item($i,3)=$currVersion
-        $alstomDesc.Cells.Item($i,2)="N/A"
-        $alstomDesc.Cells.Item($i,2).Font.ColorIndex="3"
+        $new.Cells.Item($i,1)=$currName
+        $new.Cells.Item($i,3)=$currVersion
+        $new.Cells.Item($i,2)="N/A"
+        $new.Cells.Item($i,2).Font.ColorIndex="3"
     }
 }
 Write-Host "done"
